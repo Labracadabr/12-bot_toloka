de6254858829570ecec2fdfd0e62369ef83f5ab7
Dmitrii <dminokin@gmail.com>
2024-06-11 22:05:14 +0300

    commands on_start

--- bot.py
+++ bot.py
@@ -1,8 +1,9 @@
 import asyncio
 from aiogram.fsm.storage.memory import MemoryStorage
 from aiogram import Bot, Dispatcher
+from aiogram.types import BotCommand
 from config import config
-from handlers import admin, user
+from handlers import user
 import settings
 
 # Инициализация бота
@@ -12,15 +13,24 @@ dp: Dispatcher = Dispatcher(storage=storage)
 
 
 async def main():
-    settings.check_files()
     # Регистрируем роутеры в диспетчере
     dp.include_router(user.router)
-    # dp.include_router(admin.router)
-    # dp.include_router(test.router)
+    await on_start(bot=bot)
 
     # polling
     await bot.delete_webhook()
     await dp.start_polling(bot)
+
+# при запуске: создать команды в меню и написать ссылку в консоль
+async def on_start(bot: Bot) -> None:
+    command_list = [BotCommand(command=item[0], description=item[1]) for item in settings.commands.items()]
+    await bot.set_my_commands(commands=command_list)
+    print('Команды созданы:', len(command_list), 'шт')
+
+    # ссылка на бота
+    bot_info = await bot.get_me()
+    bot_link = f"https://t.me/{bot_info.username}"
+    print(f'{bot_link = }')
 
 
 if __name__ == '__main__':
--- settings.py
+++ settings.py
@@ -1,8 +1,4 @@
-import os
-import json
-
 # tg канал для логов
-# log_channel_id = ''
 log_channel_id = '-1002130916129'
 
 # Список id админов
@@ -12,16 +8,9 @@ admins: list[str] = [dima]
 # где хранятся данные
 logs = 'logs.tsv'
 
-def check_files():
-    file_list = [logs]
-    for file in file_list:
-        if not os.path.isfile(file):
-            if file.endswith('json'):
-                with open(file, 'w', encoding='utf-8') as f:
-                    print('Отсутствующий файл создан:', file)
-                    print('{}', file=f)
-            elif file.endswith('sv'):
-                with open(file, 'w', encoding='utf-8') as f:
-                    print('Отсутствующий файл создан:', file)
-                    print('\t'.join(('Time', 'User', 'Action')), file=f)
-    print('OK')
+# команды бота
+commands = {
+    "/url_test": "Создать пул тестов",
+    "/check_html": "Проверка полей ввода",
+    "/help": "Помощь",
+}
